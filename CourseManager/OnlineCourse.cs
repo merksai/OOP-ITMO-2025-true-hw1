ef1608a88d44b77fb0c3bb62bff2a9ff1ce278dc
merksai <merkksaii@gmail.com>
2025-12-19 06:43:51 +0300

    Update OnlineCourse.cs

--- CourseManager/OnlineCourse.cs
+++ CourseManager/OnlineCourse.cs
@@ -7,19 +7,15 @@ namespace CourseManager
         public string Platform { get; }
         public string Url { get; }
 
-        public OnlineCourse(string title, string platform, string url)
-            : base(title)
+        public OnlineCourse(string title, string platform, string url) : base(title)
         {
-            if (string.IsNullOrWhiteSpace(platform))
-                throw new ArgumentException("Платформа не может быть пустой", nameof(platform));
-
-            if (string.IsNullOrWhiteSpace(url))
-                throw new ArgumentException("URL не может быть пустым", nameof(url));
-
             Platform = platform;
             Url = url;
         }
 
-        protected override string GetCourseTypeName() => "Онлайн-курс";
+        public override string GetCourseTypeName()
+        {
+            return "Онлайн-курс";
+        }
     }
 }
