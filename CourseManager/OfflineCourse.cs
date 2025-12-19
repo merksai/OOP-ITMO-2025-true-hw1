6f8a921441d937593ee1be6f58ee4ce7bf26ad58
merksai <merkksaii@gmail.com>
2025-12-19 06:43:32 +0300

    Update OfflineCourse.cs

--- CourseManager/OfflineCourse.cs
+++ CourseManager/OfflineCourse.cs
@@ -7,19 +7,15 @@ namespace CourseManager
         public string Classroom { get; }
         public string Building { get; }
 
-        public OfflineCourse(string title, string classroom, string building)
-            : base(title)
+        public OfflineCourse(string title, string classroom, string building) : base(title)
         {
-            if (string.IsNullOrWhiteSpace(classroom))
-                throw new ArgumentException("Аудитория не может быть пустой", nameof(classroom));
-
-            if (string.IsNullOrWhiteSpace(building))
-                throw new ArgumentException("Корпус не может быть пустым", nameof(building));
-
             Classroom = classroom;
             Building = building;
         }
 
-        protected override string GetCourseTypeName() => "Офлайн-курс";
+        public override string GetCourseTypeName()
+        {
+            return "Офлайн-курс";
+        }
     }
 }
